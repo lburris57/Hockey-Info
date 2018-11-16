baa5c6e49240ab0935e27c2b1e48446466da0577
Larry Burris <lburris@mac.com>
2018-11-15 21:09:49 -0500

    Added score custom cell for iPhone

---
+++ ScoreCell.swift
@@ -0,0 +1,41 @@
+//
+//  ScoreCell.swift
+//  Hockey Info
+//
+//  Created by Larry Burris on 11/15/18.
+//  Copyright © 2018 Larry Burris. All rights reserved.
+//
+
+import UIKit
+
+class ScoreCell: UITableViewCell
+{
+    @IBOutlet weak var timeRemaining: UITextField!
+    
+    @IBOutlet weak var visitingTeamName: UITextField!
+   
+    @IBOutlet weak var visitingTeamRecord: UITextField!
+    
+    @IBOutlet weak var homeTeamName: UITextField!
+    
+    @IBOutlet weak var homeTeamRecord: UITextField!
+    
+    @IBOutlet weak var visitingTeamScore: UITextField!
+    
+    @IBOutlet weak var homeTeamScore: UITextField!
+    
+    @IBOutlet weak var period: UITextField!
+    
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
