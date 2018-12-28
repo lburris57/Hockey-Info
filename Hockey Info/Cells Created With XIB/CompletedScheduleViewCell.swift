530bbe11f3e90f3f21528165c57cdfec08559d73
Larry Burris <lburris@mac.com>
2018-12-27 19:34:45 -0500

    Added custom cells to display completed/future schedule data for a specific team.

---
+++ Hockey Info/Cells Created With XIB/CompletedScheduleViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CompletedScheduleViewCell.swift
+//  Hockey Info
+//
+//  Created by Larry Burris on 12/27/18.
+//  Copyright © 2018 Larry Burris. All rights reserved.
+//
+import UIKit
+
+class CompletedScheduleViewCell: UITableViewCell
+{
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var opponent: UILabel!
+    @IBOutlet weak var result: UILabel!
+    
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+    }
+}
